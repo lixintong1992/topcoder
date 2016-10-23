afa394d71fa6b56bfdfc45efae108b0f8bc76374
lixintong <18392181430@163.com>
2016-10-23 14:30:45 +0800

    need practice

---
+++ SRM_DIV2/java/144_1100.java
@@ -0,0 +1,26 @@
+// import java.util.*
+public class PowerOutage{
+        int[] f;
+        int[] t;
+        int[] d;
+        int dfs(int s){
+            int res = 0;
+            for (int i = 0; i < f.length; i++){
+                if (f[i] == s){
+                    res = Math.max(res, dfs(t[i]) + d[i]);
+                }
+            }
+            return res;
+        }
+    public int estimateTimeOut(int[] fromJunction, int[] toJunction, int[] ductLength){
+        f = fromJunction;
+        t = toJunction;
+        d = ductLength;
+        int n = fromJunction.length;
+        int total = 0;
+        for (int i = 0; i < n; i++){
+            total += 2*d[i];
+        }
+        return total - dfs(0);
+    }
+}

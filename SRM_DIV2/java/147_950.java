7728b35962d7ac630724a1c2265770fe9003ee34
lixintong <18392181430@163.com>
2016-10-24 18:25:03 +0800

    fighting

--- SRM_DIV2/java/147_950.java
+++ SRM_DIV2/java/147_950.java
@@ -5,8 +5,11 @@ public class GoldenChain{
         int cuts = 0;
         Arrays.sort(sections);
         for( int i = 0; i < sections.length; i++){
-            if (sections.length - i - 1 >= sections[i] + cuts)cuts += sections[i];
-            else cuts += Math.max(0, sections.length - cuts -i);
+            if (sections.length - 1 - i >= sections[i] + cuts)cuts += sections[i];
+            else{
+                cuts += Math.max(0, sections.length - cuts -i);
+                break;
+            }
         }
         return cuts;
     }
